433b3dc8efe8efccf9ee92c97e9f0a605d813b7a
“Olena <a4olena@gmail.com>
2023-08-07 13:42:28 +0300

    main models

---
+++ TASK-06/school_lesson_6_2/models/library_author.py
@@ -0,0 +1,23 @@
+from odoo import fields, models
+
+
+class LibraryAuthor(models.Model):
+    _name = 'library.author'
+    _description = 'Library Book Authors'
+
+    first_name = fields.Char(required=True, translate=True)
+    last_name = fields.Char(required=True, translate=True)
+    birth_date = fields.Date('Birthday')
+    country = fields.Char('Country')
+
+    def name_get(self):
+        return [(rec.id, "%s %s" % (
+            rec.first_name, rec.last_name)) for rec in self]
+
+    def action_delete(self):
+        self.ensure_one()
+        self.check_access_rights('unlink')
+        self.unlink()
+
+    def _create_by_user(self, vals):
+        return self.sudo().create(vals)
